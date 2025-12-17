628bcce12d922c8ce6fd5e70c3592f2be759ced7
Richard Sebold <138056060+richardsebold@users.noreply.github.com>
2025-12-17 12:51:18 -0300

    adiciona funcionalidades de visualização e mensagem para negociações

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -2,21 +2,28 @@ export class Negociacao {
     // private _data: Date;
     // private _quantidade: number;
     // private _valor: number;
-    constructor(_data, _quantidade, _valor) {
+    // private _data: Date,
+    // private _quantidade: number,
+    // private _valor: number) {}
+    // get data(): Date {
+    //     return this._data
+    // }
+    // get quantidade(): number {
+    //     return this._quantidade
+    // }
+    // get valor(): number {
+    //     return this._valor
+    // }
+    constructor(_data, quantidade, valor) {
         this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
+    }
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
