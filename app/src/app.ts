a0871e007a97ed324af71bad52757227e8e90295
Richard Sebold <138056060+richardsebold@users.noreply.github.com>
2025-12-21 22:37:56 -0300

    adiciona botão de importação e implementa método para importar dados no NegociacaoController pela API fetch

--- app/src/app.ts
+++ app/src/app.ts
@@ -11,4 +11,14 @@ if (form) {
     throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
 }
 
+const botaoImporta = document.querySelector('#botao-importa');
+
+if (botaoImporta) {
+    form.addEventListener('click', () => {
+        controller.importaDados();
+    });
+} else {
+    throw Error('Botao importa nao encontrado.');
+}
+
 
